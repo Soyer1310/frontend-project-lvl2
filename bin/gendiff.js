e19c0223d79a066d7be09796fd9e2d75f163cdc8
Soyer1310 <soyer131313@gmail.com>
2022-06-07 22:56:02 +0300

    fix version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('1.0.0');
 
 program.parse();
