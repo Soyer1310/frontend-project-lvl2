5aed159fcf86e4c20c262961e240d0d57aeedaa3
Soyer1310 <soyer131313@gmail.com>
2022-06-30 22:32:20 +0300

    modify tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js';
 import { differentFiles, equalFiles, emptyFiles } from '../__fixtures__/expected.file.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,29 +10,35 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe('JSON tests', () => {
-  test('gendiff of two defferent flat JSONs', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(differentFiles);
+  test('gendiff of two defferent JSONs', () => {
+    const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+    expect(stylish(diff)).toEqual(differentFiles);
   });
 
   test('gendiff with two equal JSONs', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file3.json'))).toEqual(equalFiles);
+    const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file3.json'));
+    expect(stylish(diff)).toEqual(equalFiles);
   });
 
   test('gendiff with two empty JSONs', () => {
-    expect(genDiff(getFixturePath('empty1.json'), getFixturePath('empty2.json'))).toEqual(emptyFiles);
+    const diff = genDiff(getFixturePath('empty1.json'), getFixturePath('empty2.json'))
+    expect(stylish(diff)).toEqual(emptyFiles);
   });
 });
 
 describe('YAML tests', () => {
   test('gendiff of two defferent flat YAML', () => {
-    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(differentFiles);
+    const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))
+    expect(stylish(diff)).toEqual(differentFiles);
   });
 
   test('gendiff with two equal YAML', () => {
-    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file3.yaml'))).toEqual(equalFiles);
+    const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file3.yaml'))
+    expect(stylish(diff)).toEqual(equalFiles);
   });
 
   test('gendiff with two empty YAML', () => {
-    expect(genDiff(getFixturePath('empty1.yaml'), getFixturePath('empty2.yaml'))).toEqual(emptyFiles);
+    const diff = genDiff(getFixturePath('empty1.yaml'), getFixturePath('empty2.yaml'))
+    expect(stylish(diff)).toEqual(emptyFiles);
   });
 });
