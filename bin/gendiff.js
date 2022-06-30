5bc86eebd32c01786a975bae247630e1ca03f293
Soyer1310 <soyer131313@gmail.com>
2022-06-30 22:33:31 +0300

    add default option stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
+import stylish from '../src/stylish.js'
 
 const program = new Command();
-
+let result;
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1>')
   .arguments('<filepath1>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    result = genDiff(filepath1, filepath2)
+  });
 program.parse();
+
+const options = program.opts();
+if (options.format) {
+  console.log(stylish(result));
+}
