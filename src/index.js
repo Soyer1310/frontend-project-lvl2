16988f12f429a243eac26d73d632975cd7f3b02e
Soyer1310 <soyer131313@gmail.com>
2022-07-05 22:02:57 +0300

    modify src/index.js for plain for format selection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import readFile from './readFile.js';
 import genPath from './genPath.js';
 import compareFiles from './compareFiles.js';
+import formatted from '../formatters/index.js'
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, format) => {
   const file1 = readFile(genPath(filepath1));
   const file2 = readFile(genPath(filepath2));
-  const diffStr = compareFiles(file1, file2);
-  return diffStr;
+  const diff = compareFiles(file1, file2);
+  const formatedDiff = formatted(diff, format);
+  return formatedDiff;
 };
