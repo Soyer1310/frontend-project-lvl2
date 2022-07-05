540558d170eceadc3e619dbccc105d22900ab942
Soyer1310 <soyer131313@gmail.com>
2022-07-05 22:00:50 +0300

    modify gendiff.js for plain format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import plain from '../formatters/plain.js';
 import genDiff from '../src/index.js';
-import stylish from '../src/stylish.js';
+import stylish from '../formatters/stylish.js';
 
 const program = new Command();
-let result;
+let diff;
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
@@ -13,11 +14,13 @@ program
   .arguments('<filepath1>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    result = genDiff(filepath1, filepath2);
+    diff = genDiff(filepath1, filepath2);
   });
 program.parse();
 
 const options = program.opts();
-if (options.format) {
-  console.log(stylish(result));
+if (options.format === 'stylish') {
+  console.log(stylish(diff));
+} else if (options.format === 'plain') {
+  console.log(plain(diff));
 }
