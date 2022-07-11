da9301b17d77d6cd63da2ad3b714e07d27460b6c
Soyer1310 <soyer131313@gmail.com>
2022-07-11 21:52:05 +0300

    fix formaters calling in index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import readFile from './src/readFile.js';
+import genPath from './src/genPath.js';
+import compareFiles from './src/compareFiles.js';
+import formatted from './src/formatters/index.js';
+
+export default (filepath1, filepath2, format) => {
+  const file1 = readFile(genPath(filepath1));
+  const file2 = readFile(genPath(filepath2));
+  const diff = compareFiles(file1, file2);
+  let currentFormat = format;
+  if(format.format) {
+    currentFormat = format.format;
+  }
+  const formatedDiff = formatted(diff, currentFormat);
+  return formatedDiff;
+};
