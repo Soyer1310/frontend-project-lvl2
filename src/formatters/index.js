5d848f7282034808a2d53b505c1acb5349555d16
Soyer1310 <soyer131313@gmail.com>
2022-07-11 21:54:10 +0300

    fix imports pathes

---
+++ src/formatters/index.js
@@ -0,0 +1,16 @@
+import stylish from './stylish.js';
+import plain from './plain.js';
+import toJson from './json.js';
+
+export default (diff, format) => {
+  let formatedDiff = diff;
+  if (format === 'stylish') {
+    formatedDiff = stylish(diff);
+  } else if (format === 'plain') {
+    formatedDiff = plain(diff);
+  } else if (format === 'json') {
+    formatedDiff = toJson(diff);
+  }
+
+  return formatedDiff;
+};
---
+++ src/formatters/plain.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+
+const plain = (diff) => {
+  const iter = (node, previousKey) => {
+    const lines = Object.entries(node).map((item) => {
+      const [key, value] = item;
+      if (value.children) {
+        let nextKey;
+        if (previousKey === '') {
+          nextKey = `${key}.`;
+        } else {
+          nextKey = `${previousKey}${key}.`;
+        }
+        return iter(value.children, nextKey);
+      }
+
+      let { deleted } = value;
+      if (typeof deleted === 'string') {
+        deleted = `'${deleted}'`;
+      }
+
+      if (_.isObject(deleted)) {
+        deleted = '[complex value]';
+      }
+
+      let { added } = value;
+      if (typeof added === 'string') {
+        added = `'${added}'`;
+      }
+
+      if (_.isObject(added)) {
+        added = '[complex value]';
+      }
+
+      if (value.type === 'different') {
+        return `Property '${previousKey}${key}' was updated. From ${deleted} to ${added}\n`;
+      } if (value.type === 'first') {
+        return `Property '${previousKey}${key}' was removed\n`;
+      } if (value.type === 'second') {
+        return `Property '${previousKey}${key}' was added with value: ${added}\n`;
+      }
+      return null;
+    });
+    return [
+      ...lines,
+    ].join('');
+  };
+  return iter(diff, '');
+};
+
+export default plain;
