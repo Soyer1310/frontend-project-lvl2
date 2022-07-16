d45715a63656a53f41e77fedf8c6ff4276b01db1
Soyer1310 <soyer131313@gmail.com>
2022-07-16 15:17:49 +0300

    delete constant diff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,6 @@ import { Command } from 'commander';
 import genDiff from '../index.js';
 
 const program = new Command();
-let diff;
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
@@ -15,4 +14,3 @@ program
     console.log(genDiff(filepath1, filepath2, type));
   });
 program.parse();
-
