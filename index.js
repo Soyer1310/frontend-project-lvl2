6e2f99680a09569ad29a6d1c7e2f324ab0715e3f
Soyer1310 <soyer131313@gmail.com>
2022-07-16 15:19:55 +0300

    unite files index.js getPath.js and readFile.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import _ from 'lodash';
-import readFile from './src/readFile.js';
-import genPath from './src/genPath.js';
+import fs from 'fs';
+import path from 'path';
+import { cwd } from 'process';
+import parsers from './src/parsers.js';
 import compareFiles from './src/compareFiles.js';
 import formatted from './src/formatters/index.js';
+
+const readFile = (filePath) => {
+  const content = fs.readFileSync(filePath, 'utf8');
+  const format = path.extname(filePath);
+  return parsers(content, format);
+};
+
+const genPath = (processedPath) => {
+  const currentDir = cwd();
+  return path.resolve(currentDir, processedPath);
+};
 
 export default (filepath1, filepath2, format) => {
   const file1 = readFile(genPath(filepath1));
   const file2 = readFile(genPath(filepath2));
   const diff = compareFiles(file1, file2);
   let currentFormat = format;
-  if(_.isObject(format)) {
+  if (_.isObject(format)) {
     currentFormat = format.format;
   }
   const formatedDiff = formatted(diff, currentFormat);
