e5a372412e88e0022182f91ce49f1c34c7621dc6
Soyer1310 <soyer131313@gmail.com>
2022-08-28 22:04:04 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,65 +1,44 @@
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
-import {
-  differentFiles, equalFiles, emptyFiles, plainFiles, jsonStr,
-} from '../src/expected.file.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('JSON tests', () => {
   test('gendiff with two defferent JSONs (stylish formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
-    expect(diff).toEqual(differentFiles);
-  });
-
-  test('gendiff with two equal JSONs (stylish formatter)', () => {
-    const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file3.json'), 'stylish');
-    expect(diff).toEqual(equalFiles);
-  });
-
-  test('gendiff with two empty JSONs (stylish formatter)', () => {
-    const diff = genDiff(getFixturePath('empty1.json'), getFixturePath('empty2.json'), 'stylish');
-    expect(diff).toEqual(emptyFiles);
+    expect(diff).toEqual(readFile('stylish.txt'));
   });
 
   test('gendiff JSONs (plain formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
-    expect(diff).toEqual(plainFiles);
+    expect(diff).toEqual(readFile('plain.txt'));
   });
 
   test('gendiff JSONs (json formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-    expect(diff).toEqual(jsonStr);
+    expect(diff).toEqual(readFile('json.txt'));
   });
 });
 
 describe('YAML tests', () => {
   test('gendiff of two defferent YAML (stylish formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish');
-    expect(diff).toEqual(differentFiles);
-  });
-
-  test('gendiff with two equal YAML (stylish formatter)', () => {
-    const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file3.yaml'), 'stylish');
-    expect(diff).toEqual(equalFiles);
-  });
-
-  test('gendiff with two empty YAML (stylish formatter)', () => {
-    const diff = genDiff(getFixturePath('empty1.yaml'), getFixturePath('empty2.yaml'), 'stylish');
-    expect(diff).toEqual(emptyFiles);
+    expect(diff).toEqual(readFile('stylish.txt'));
   });
 
   test('gendiff YAML with (plain formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain');
-    expect(diff).toEqual(plainFiles);
+    expect(diff).toEqual(readFile('plain.txt'));
   });
 
   test('gendiff YAMLs with (json formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json');
-    expect(diff).toEqual(jsonStr);
+    expect(diff).toEqual(readFile('json.txt'));
   });
 });
