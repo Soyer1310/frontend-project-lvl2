7dfcb0ebd4884eabca8207c99244c8421ade790e
Soyer1310 <soyer131313@gmail.com>
2022-09-14 20:34:14 +0300

    fix txt reading

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,37 +8,40 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const stylish = readFile('stylish.txt');
+const plain = readFile('plain.txt');
+const json = readFile('json.txt');
 
 describe('JSON tests', () => {
   test('gendiff with two defferent JSONs (stylish formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
-    expect(diff).toEqual(readFile('stylish.txt'));
+    expect(diff).toEqual(stylish);
   });
 
   test('gendiff JSONs (plain formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
-    expect(diff).toEqual(readFile('plain.txt'));
+    expect(diff).toEqual(plain);
   });
 
   test('gendiff JSONs (json formatter)', () => {
     const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-    expect(diff).toEqual(readFile('json.txt'));
+    expect(diff).toEqual(json);
   });
 });
 
 describe('YAML tests', () => {
   test('gendiff of two defferent YAML (stylish formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish');
-    expect(diff).toEqual(readFile('stylish.txt'));
+    expect(diff).toEqual(stylish);
   });
 
   test('gendiff YAML with (plain formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain');
-    expect(diff).toEqual(readFile('plain.txt'));
+    expect(diff).toEqual(plain);
   });
 
   test('gendiff YAMLs with (json formatter)', () => {
     const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json');
-    expect(diff).toEqual(readFile('json.txt'));
+    expect(diff).toEqual(json);
   });
 });
