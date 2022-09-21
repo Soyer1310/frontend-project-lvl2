057add364c958951df0dd2d12fc31abdccd84962
Soyer1310 <soyer131313@gmail.com>
2022-09-21 20:59:42 +0300

    fix name of import (format)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import parsers from './parsers.js';
 import compareData from './compareData.js';
-import formatted from './formatters/index.js';
+import format from './formatters/index.js';
 
 const getData = (filePath) => {
   const data = fs.readFileSync(filePath, 'utf8');
@@ -15,10 +15,10 @@ const genPath = (processedPath) => {
   return path.resolve(currentDir, processedPath);
 };
 
-export default (filepath1, filepath2, format = 'stylish') => {
+export default (filepath1, filepath2, formatter = 'stylish') => {
   const data1 = getData(genPath(filepath1));
   const data2 = getData(genPath(filepath2));
   const diff = compareData(data1, data2);
-  const formatedDiff = formatted(diff, format);
+  const formatedDiff = format(diff, formatter);
   return formatedDiff;
 };
